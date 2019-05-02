1b7b61ef0b48f0cf970243ea7db10708a58269cc
Wehner <veit.wehner@siemens.com>
2019-05-02 17:13:42 +0200

    self made good looking toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
 <template>
   <v-app>
-     <!-- <v-card color="basil">
-         <v-card-title class="text-xs-center justify-center py-4">
+    <v-card color="basil">
+      <v-card-title class="text-xs-center justify-center py-4">
         <h1 class="font-weight-bold display-3 basil--text">BASiL</h1>
-      </v-card-title> -->
-    <v-toolbar color="cyan" dark tabs>
+      </v-card-title>
+      <v-tabs v-model="tab" color="basil" grow>
+        <v-tab v-for="item in items" :key="item">{{ item }}</v-tab>
+      </v-tabs>
+
+      <v-tabs-items v-model="tab">
+        <v-tab-item v-for="item in items" :key="item">
+          <v-card flat color="basil">
+            <v-card-text>{{ text }}</v-card-text>
+          </v-card>
+        </v-tab-item>
+      </v-tabs-items>
+    </v-card>
+    <!-- <v-toolbar color="cyan" dark tabs>
       <v-toolbar-side-icon></v-toolbar-side-icon>
       <v-toolbar-title>Veit Wehner's Place</v-toolbar-title>
       <template v-slot:extension>
@@ -22,7 +34,7 @@
       </v-tab-item>
     </v-tabs-items>
     <!-- <core-toolbar /> -->
-    <core-view/>
+    <core-view/> -->
     <!-- <core-footer /> -->
   </v-app>
 </template>
