e03c6f8eeac1e438c4e8b003031edc1bc4002797
Wehner <veit.wehner@siemens.com>
2019-05-04 14:17:19 +0200

    two ways to scroll the page with the router

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,13 @@ import Vue from 'vue'
 import './plugins/vuetify'
 import './plugins/base'
 import App from './App.vue'
-import store from './store'
+import router from "./router/router"
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import VueYouTubeEmbed from 'vue-youtube-embed'
 Vue.use(VueYouTubeEmbed)
 Vue.config.productionTip = false
 
 new Vue({
-  store,
+  router,
   render: h => h(App)
 }).$mount('#app')
--- src/router/router.ts
+++ src/router/router.ts
@@ -1,17 +1,32 @@
 import Vue from "vue";
 import { RouterOptions, RouteConfig } from "vue-router";
-import Contact from "@/components/Contact.vue";
-import Home from "../views/home/Home.vue";
+import Carbon from "@/components/Carbon.vue";
 import Router from "vue-router";
+import VueScrollTo from "vue-scrollto"; // Use VueScrollTo for animation.
 
 Vue.use(Router);
 
 const navRoutes = <Array<RouteConfig>>[
-  { name: "home", path: "", component: Home },
+  { name: "carbon", path: "/", component: Carbon },
 ];
+
+var scrollBehavior = function(to, from, savedPosition) {
+  console.log("scrolling..");
+  if (to.hash) {
+    return {
+      selector: to.hash
+    }
+  } else {
+    return {
+      x: 0,
+      y: 0
+    }
+  }
+};
 
 const router = new Router({
   mode: "hash",
+  scrollBehavior: scrollBehavior,
   routes: navRoutes
 });
 
