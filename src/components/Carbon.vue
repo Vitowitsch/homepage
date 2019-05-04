fe5bc36986734709b6f4153d1dfb600fdf3ec6e2
Wehner <veit.wehner@siemens.com>
2019-05-04 09:20:44 +0200

    ++

--- src/components/Carbon.vue
+++ src/components/Carbon.vue
@@ -1,38 +1,132 @@
   <template>
-  <v-container justify-space-between>
-    <v-layout>
-      <div class="display-4">Forget Bitcoin - Blockchain can help reduce CO₂-emissions</div>
-    </v-layout>
-    <v-layout mt-5 mb-5>
-      <v-img :src="require('@/assets/pics/globe.jpg')" aspect-ratio="3.0" contain/>
-    </v-layout>
-    <v-layout wrap justify-space-around>
-      <v-flex xs12>
-        <base-text mb-5>
-          A predictive pricing for carbon certificates was awarded first prize at the Siemens-AI-Meets-Blockchain-Hackathon.
-          In the 2016 Paris climate treaty the world's peoples decided to strictly regulate Carbon Dioxide emissions to reach our climate goals: to prevent the average temperature from warming up by more than 2° degrees till 2100.
-          To support the world-wide reductions, an emission-trading-system was established, where states can grant the rights to emit a certain amount of CO₂ into the atmosphere. However, since the price of those certificates is not aligned to the climate-goals, this system has contributed little to real emission reduction.
-          At last year's Siemens-Hackathon in the Munich AI-Lab we invented an AI-based pricing model which includes current climate goals, by factoring current CO₂ emissions and necessary reductions into the certificate price. The trading system itself is managed by a Smart-Contract-Blockchain, controlling number and price of certificates. A company that works efficiently and has less CO₂ emissions than planned, can sell their rights to another company with higher needs, via the blockchain.
-          We chose Blockchain as a trading-platform as it is transparent: Everybody knows who the rightful certificate owner is.
-          It is also flexible, meaning you can adapt the pricing-model to new situations and politics. As the system and climate data updates are fully automated by the Chain-Code, it only requires few staff to manage it.
-          Blockchain itself can use up a lot of energy and create a huge amount of CO₂ emissions. That is something we want to avoid. Therefore we decided on a different consensus strategy than the traditional proof of work approach, which is used in the infamous bitcoin mining farms: Hyperledger's permission-based algorithm runs on standard enterprise servers that don't require many ressources - neither hardware nor energy.
-          Theoretically this idea could generate a lot of money. The commission for world-wide trade could bring up to two billion a year. This is a huge number and - we have to admit - too large a project for us to realise. We see a better trading system as a viable way to reduce CO₂ emission.
-          By sharing the concept with different people in politics and within Siemens we hope to initiate changes - big or small.
-          <p></p>Team Carbon: Elena Scepankova, Veit Wehner, Johannes Oberreuter
-        </base-text>
-      </v-flex>
-      <v-flex xs12 mt-5>
-        <v-img :src="require('@/assets/pics/carbon_.jpeg')" aspect-ratio="3.0" contain/>
-      </v-flex>
-    </v-layout>
-  </v-container>
+  <article>
+    <v-chart :options="options"/>
+    <v-container justify-space-between>
+      <v-layout>
+        <div class="display-4">Forget Bitcoin - Blockchain can help reduce CO₂-emissions</div>
+      </v-layout>
+      <v-layout xs12 mt-5 mb-5>
+        <v-flex></v-flex>
+        <!-- <v-img :src="require('@/assets/pics/globe.jpg')" aspect-ratio="3.0" contain/> -->
+      </v-layout>
+      <v-layout wrap justify-space-around>
+        <v-flex xs12>
+          <v-chart :options="globe"/>
+          <base-text mb-5>
+            A predictive pricing for carbon certificates was awarded first prize at the Siemens-AI-Meets-Blockchain-Hackathon.
+            In the 2016 Paris climate treaty the world's peoples decided to strictly regulate Carbon Dioxide emissions to reach our climate goals: to prevent the average temperature from warming up by more than 2° degrees till 2100.
+            To support the world-wide reductions, an emission-trading-system was established, where states can grant the rights to emit a certain amount of CO₂ into the atmosphere. However, since the price of those certificates is not aligned to the climate-goals, this system has contributed little to real emission reduction.
+            At last year's Siemens-Hackathon in the Munich AI-Lab we invented an AI-based pricing model which includes current climate goals, by factoring current CO₂ emissions and necessary reductions into the certificate price. The trading system itself is managed by a Smart-Contract-Blockchain, controlling number and price of certificates. A company that works efficiently and has less CO₂ emissions than planned, can sell their rights to another company with higher needs, via the blockchain.
+            We chose Blockchain as a trading-platform as it is transparent: Everybody knows who the rightful certificate owner is.
+            It is also flexible, meaning you can adapt the pricing-model to new situations and politics. As the system and climate data updates are fully automated by the Chain-Code, it only requires few staff to manage it.
+            Blockchain itself can use up a lot of energy and create a huge amount of CO₂ emissions. That is something we want to avoid. Therefore we decided on a different consensus strategy than the traditional proof of work approach, which is used in the infamous bitcoin mining farms: Hyperledger's permission-based algorithm runs on standard enterprise servers that don't require many ressources - neither hardware nor energy.
+            Theoretically this idea could generate a lot of money. The commission for world-wide trade could bring up to two billion a year. This is a huge number and - we have to admit - too large a project for us to realise. We see a better trading system as a viable way to reduce CO₂ emission.
+            By sharing the concept with different people in politics and within Siemens we hope to initiate changes - big or small.
+            <p></p>Team Carbon: Elena Scepankova, Veit Wehner, Johannes Oberreuter
+          </base-text>
+        </v-flex>
+        <v-flex xs12 mt-5>
+          <v-img :src="require('@/assets/pics/carbon_.jpeg')" aspect-ratio="3.0" contain/>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </article>
 </template>
 <script>
+import ECharts from "vue-echarts";
+import "echarts-gl";
+// import "echarts-gl/lib/chart/treemap";
 export default {
+  components: {
+    "v-chart": ECharts
+  },
   data() {
     return {
-      e1: 0
+      options: {
+        tooltip: {},
+        visualMap: {
+          show: false,
+          dimension: 2,
+          min: -1,
+          max: 1,
+          inRange: {
+            color: [
+              "#e0f3f8",
+              "#ffffbf",
+              "#fee090",
+              "#fdae61",
+              "#f46d43",
+              "#d73027",
+              "#a50026"
+            ]
+          }
+        },
+        xAxis3D: {},
+        yAxis3D: {},
+        zAxis3D: {},
+        grid3D: {},
+        series: [
+          {
+            type: "surface",
+            parametric: true,
+            parametricEquation: {
+              u: {
+                min: -Math.PI,
+                max: Math.PI,
+                step: Math.PI / 20
+              },
+              v: {
+                min: 0,
+                max: Math.PI,
+                step: Math.PI / 20
+              },
+              x: function(u, v) {
+                return Math.sin(v) * Math.sin(u);
+              },
+              y: function(u, v) {
+                return Math.sin(v) * Math.cos(u);
+              },
+              z: function(u, v) {
+                return Math.cos(v);
+              }
+            }
+          }
+        ]
+      },
+      globe: {
+        backgroundColor: "#000",
+        globe: {
+          baseTexture: "assets/pics/world.topo.bathy.200401.jpg",
+          heightTexture: "assets/pics/world.topo.bathy.200401.jpg",
+          displacementScale: 0.04,
+          shading: "realistic",
+          environment: "assets/pics/starfield.jpg",
+          realisticMaterial: {
+            roughness: 0.9
+          },
+          postEffect: {
+            enable: true
+          },
+          light: {
+            main: {
+              intensity: 5,
+              shadow: true
+            },
+            ambientCubemap: {
+              texture: "assets/pics/pisa.hdr",
+              diffuseIntensity: 0.2
+            }
+          }
+        }
+      }
     };
   }
 };
 </script>
+<style>
+.echarts {
+  width: 100%;
+  height: 20%;
+}
+</style>
+
--- src/components/CityToll.vue
+++ src/components/CityToll.vue
@@ -1,46 +1,49 @@
   <template>
-  <v-container justify-space-between>
-    <v-layout>
-      <div class="display-4">Pollution is in the air</div>
-    </v-layout>
-    <v-layout mt-5 mb-5>
-      <div class="display-2">A smart city toll system (Cito) made second prizes</div>
-    </v-layout>
-    <v-layout wrap justify-space-around>
-      <v-flex xs12>
-        <base-text mb-5>
-          The estimated number of premature deaths due to traffic related air pollution is estimated by 60 thousand per year in Germany. Even if this number is controversial, 65 German cities surpassed the pollution levels in 2017. If they all were fined by the EU, this would add up to millions of Euros.
-          <p></p>Traffic related pollution was one of the topics at the 2018’s <a href="https://www.bmu.de/service/veranstaltungen/wettbewerbe/code4green">Code4Green</a> hackathon at the ministry of environment. It took place in the ministry itself that had been perfectly equipped with beds and all the stuff you need for a 24h hackathon. Even Minister Svenja Schulze herself showed up.
-          <p></p>I was part of a team of seven. The idea for the final project only emerged the night before the hackathon: We built a smart system that calculates dynamic price for entering - or parking - in the city, based on the real time NOx level. It should also include a 24h hours forecast. There were two options for billing: Either an IOT device on the car that would automatically deduct the payment from a prepaid account when it entered a city zone, or smart parking ticket vending machines.
-          </base-text mb-5>
-          </v-flex>
-          <v-flex xs9 mt-5 mb-5>
-           <v-img :src="require('@/assets/pics/code4green-device.jpg')" aspect-ratio="2.0" />
-           </v-flex>
-           <v-flex>
-           <base-text mb-5>
-          <p></p>Our hackathon IoT-prototype retrieves real time and forecast pollution data from our cloud service. The city toll is deducted from a prepaid account, based on GPS and NOx level. Users can see their account balance, a 24 hour pollution forecast with corresponding prices on their mobile phones.
-          <p></p>We hope that this is a first step towards fewer cars and better air in cities. Cito can enforce legal pollution thresholds and bans on diesel cars - without tracking any personal GPS data.
-          <p></p>Let us turn the grey sky blue again!
-        </base-text>
+  <article>
+    <v-container justify-space-between>
+      <v-layout>
+        <div class="display-4">Pollution is in the air</div>
+      </v-layout>
+      <v-layout mt-5 mb-5>
+        <div class="display-2">A smart city toll system (Cito) made second prizes</div>
+      </v-layout>
+      <v-layout wrap justify-space-around>
+        <v-flex xs12>
+          <base-text mb-5>
+            The estimated number of premature deaths due to traffic related air pollution is estimated by 60 thousand per year in Germany. Even if this number is controversial, 65 German cities surpassed the pollution levels in 2017. If they all were fined by the EU, this would add up to millions of Euros.
+            <p></p>Traffic related pollution was one of the topics at the 2018’s
+            <a href="https://www.bmu.de/service/veranstaltungen/wettbewerbe/code4green">Code4Green</a> hackathon at the ministry of environment. It took place in the ministry itself that had been perfectly equipped with beds and all the stuff you need for a 24h hackathon. Even Minister Svenja Schulze herself showed up.
+            <p></p>I was part of a team of seven. The idea for the final project only emerged the night before the hackathon: We built a smart system that calculates dynamic price for entering - or parking - in the city, based on the real time NOx level. It should also include a 24h hours forecast. There were two options for billing: Either an IOT device on the car that would automatically deduct the payment from a prepaid account when it entered a city zone, or smart parking ticket vending machines.
+          </base-text>
         </v-flex>
-    </v-layout>
-    <v-layout align-center mt-5>
-              <youtube :video-id="getVideoId()"></youtube>
-              </v-layout>
-  </v-container>
+        <v-flex xs9 mt-5 mb-5>
+          <v-img :src="require('@/assets/pics/code4green-device.jpg')" aspect-ratio="2.0"/>
+        </v-flex>
+        <v-flex>
+          <base-text mb-5>
+            <p></p>Our hackathon IoT-prototype retrieves real time and forecast pollution data from our cloud service. The city toll is deducted from a prepaid account, based on GPS and NOx level. Users can see their account balance, a 24 hour pollution forecast with corresponding prices on their mobile phones.
+            <p></p>We hope that this is a first step towards fewer cars and better air in cities. Cito can enforce legal pollution thresholds and bans on diesel cars - without tracking any personal GPS data.
+            <p></p>Let us turn the grey sky blue again!
+          </base-text>
+        </v-flex>
+      </v-layout>
+      <v-layout align-center mt-5>
+        <youtube :video-id="getVideoId()"></youtube>
+      </v-layout>
+    </v-container>
+  </article>
 </template>
 <script>
-import { getIdFromURL } from 'vue-youtube-embed'
+import { getIdFromURL } from "vue-youtube-embed";
 export default {
   data() {
     return {
       e1: 0
     };
   },
-  methods:{
-    getVideoId(){
-      return  getIdFromURL('https://youtu.be/rVUP4-I9TJ4');
+  methods: {
+    getVideoId() {
+      return getIdFromURL("https://youtu.be/rVUP4-I9TJ4");
     }
   }
 };
