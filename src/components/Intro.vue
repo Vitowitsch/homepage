ea74b83435147b210ec9d80ad78c77434035689b
Wehner <veit.wehner@siemens.com>
2019-09-30 19:52:14 +0200

    change photo

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -6,6 +6,14 @@
     </project-headline>
     <v-layout justify-space-between class="text-xs-left" ml-5 mr-5 wrap>
       <v-flex xs12 md6>
+        <div style="color:teal;margin-top:35px">:~$ git config --list</div>
+        <hr />user.name= Veit Wehner
+        <hr />user.DOB= 2 October 1978
+        <hr />user.made.in= Germany
+        <hr />user.location= Gerstenbergstr. 8, Erlangen, Germany
+        <hr />user.email= info@botsandbytes.de
+        <p></p>
+        <br />
         <br />Welcome!
         My name is Veit Wehner.
         <br />
@@ -15,17 +23,12 @@
         <br />With an M.Sc. in Computer Science and Mathematics and a lot of experience I have the understanding for complex requirements. Nonetheless, my motto is to keep it as simple as possible.
         <br />
         <br />At the moment I am working for Siemens in Europe, where I got excellent recommendations.
-        <br />In addition, I am looking for interesting side projects. About some of them you can read in my <router-link to="/blog" class="routerlink">blog</router-link>.
+        <br />In addition, I am looking for interesting side projects. About some of them you can read in my
+        <router-link to="/blog" class="routerlink">blog</router-link>.
         <br />
       </v-flex>
       <v-flex class="xs12 md5" pb-5 pt-5>
-        <div style="color:teal">:~$ git config --list</div>
-        <hr />user.name= Veit Wehner
-        <hr />user.DOB= 2 October 1978
-        <hr />user.made.in= Germany
-        <hr />user.location= Gerstenbergstr. 8, Erlangen, Germany
-        <hr />user.email= info@botsandbytes.de
-        <v-img :src="require('@/assets/pics/intro.jpg')" aspect-ratio="3.0"></v-img>
+        <v-img :src="require('@/assets/pics/intro.jpg')" contain aspect-ratio="1.0"></v-img>
       </v-flex>
     </v-layout>
   </center>
@@ -45,8 +48,8 @@ export default {
 };
 </script>
 <style scoped>
-.routerlink{
+.routerlink {
   color: teal;
   font-weight: bold;
-  }
+}
 </style>
