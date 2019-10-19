a88a8291222c81235181773fd415b704ec84ac4a
Wehner <veit.wehner@siemens.com>
2019-10-19 18:56:18 +0200

    vuetify upgrade and related changes

--- src/components/Certs.vue
+++ src/components/Certs.vue
@@ -5,24 +5,22 @@
       Certificates
     </project-headline>
     <v-layout justify-space-between>
-      <v-flex xs12
-        <v-card>
-          <v-container grid-list-sm fluid>
-            <v-layout wrap>
-              <v-flex v-for="p in projects" :key="p.url" xs4 d-flex>
-                <v-card flat tile class="d-flex">
-                  <expandable-image class="image" :src="p.url"></expandable-image>
-                  <template v-slot:placeholder>
-                    <v-layout fill-height align-center justify-center ma-0>
-                      <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
-                    </v-layout>
-                  </template>
-                </v-card>
-              </v-flex>
-            </v-layout>
-          </v-container>
-        </v-card>
-      </v-flex>
+      <v-container grid-list-sm fluid>
+        <v-layout wrap>
+          <v-flex v-for="p in projects" :key="p.url" xs4 d-flex>
+            <v-card flat tile class="d-flex">
+              <center>
+                <expandable-image class="image" :src="p.url"></expandable-image>
+                <template v-slot:placeholder>
+                  <v-layout fill-height align-center justify-center ma-0>
+                    <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+                  </v-layout>
+                </template>
+              </center>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
     </v-layout>
   </div>
 </template>
