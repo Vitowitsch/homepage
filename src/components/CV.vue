14e935df2c86e005a8711496a23c871a92e52bb0
Veit Wehner <sanvitoalsylvestre@gmail.com>
2023-05-22 19:04:35 +0200

    Update CV.vue

--- src/components/CV.vue
+++ src/components/CV.vue
@@ -47,13 +47,13 @@ export default {
       {
         color: "#b6b7ba",
         year: "2023",
-        text: "Special Purpose Machines. Technologies: Python, Kotlin, Quarkus, Vue.js, G Code, OpenCV, YoloV5, FastAPI, RabbitMQ, MQTT, FastAPI, Kubernetes, Gitlab.",
+        text: "Special Purpose Machines.\nTechnologies: Python, Kotlin, Quarkus, Vue.js, G Code, OpenCV, YoloV5, FastAPI, RabbitMQ, MQTT, FastAPI, Kubernetes, Gitlab.",
         title: "Technical Lead",
       },
       {
         color: "#b6b7ba",
         year: "2022 - 2023",
-        text: "Software Architect for Public-Key-Infrastructure automation. Technologies: Kotlin, Spring-Boot, BouncyCastle, CMP, Cloudformation, ECS over Fargate, GitLab, TravisCI, ShellSpec, Testcontainers, Kanban, SAFe.",
+        text: "Software Architect for Public-Key-Infrastructure automation.\nTechnologies: Kotlin, Spring-Boot, BouncyCastle, CMP, Cloudformation, ECS over Fargate, GitLab, TravisCI, ShellSpec, Testcontainers, Kanban, SAFe.",
         title: "Software Architect: Security",
       },
       {
@@ -92,38 +92,38 @@ export default {
         year: "2017 - 2020",
         title:
           "Software-Architect at Siemens - Machine-Learning, Big Data and Cloud",
-        text: "Integration of data-intense Matlab-Algorithms as Docker-Container into Amazon-AWS. Technologies:MLFlow, Kubernetes, EKS, AWS-Lambda, GitLab, Python, Machine-Learning, Statistical Learning, Neural Networks, MatLab, SimuLink, Docker, Hive, Athena, Presto, Data Warehouse, Apache Spark, ETL, Hadoop, Spotfire, EMR, AWS Lambda,Cassandra, Liquibase, Vue.js, Typescript, Javascript, GraphQL,OData, Angular, Scrum, Kanban, Microservices, Self-Contained Systems, Knowledge Graphs, Apache Kafka, Rabbit MQ, SQS, SNS, IPython (JupyterLab), SciKit Learn, Theano, MLib, Tensorflow, Pandas, Numpy, R, AWS Sagemaker. ",
+        text: "Integration of data-intense Matlab-Algorithms as Docker-Container into Amazon-AWS.\nTechnologies: MLFlow, Kubernetes, EKS, AWS-Lambda, GitLab, Python, Machine-Learning, Statistical Learning, Neural Networks, MatLab, SimuLink, Docker, Hive, Athena, Presto, Data Warehouse, Apache Spark, ETL, Hadoop, Spotfire, EMR, AWS Lambda,Cassandra, Liquibase, Vue.js, Typescript, Javascript, GraphQL,OData, Angular, Scrum, Kanban, Microservices, Self-Contained Systems, Knowledge Graphs, Apache Kafka, Rabbit MQ, SQS, SNS, IPython (JupyterLab), SciKit Learn, Theano, MLib, Tensorflow, Pandas, Numpy, R, AWS Sagemaker. ",
       },
       {
         color: "#b6b7ba",
         year: "2015 - 2017",
         title: "Software Architect at Siemens: Cloud and Big Data",
-        text: "Migration of on-premise applications into Amazon Cloud. Technologies: Java, Python, Spring-Boot, Beanstalk, EC2, MongoDB, DynamoDB, Redis, Kinesis, Graphana, Kibana, Banana, Prometheus, MariaDB, Jenkins, Travis-CI (GitLab).",
+        text: "Migration of on-premise applications into Amazon Cloud.\nTechnologies: Java, Python, Spring-Boot, Beanstalk, EC2, MongoDB, DynamoDB, Redis, Kinesis, Graphana, Kibana, Banana, Prometheus, MariaDB, Jenkins, Travis-CI (GitLab).",
       },
       {
         color: "#b6b7ba",
         year: "2011 - 2015",
         title:
           "Software Architect at Siemens: Embedded Systems, Connectivity, Web Applications",
-        text: "Shore-side connectivity of a train-sided embedded linux system to analyze train data. Technologies: Java, Facelets, Primefaces, C/C++, cmake, Jenkins.",
+        text: "Shore-side connectivity of a train-sided embedded linux system to analyze train data.\nTechnologies: Java, Facelets, Primefaces, C/C++, cmake, Jenkins.",
       },
 
       {
         color: "#b6b7ba",
         year: "2009 - 2010",
         title: "Embedded-Developer at Siemens",
-        text: "GPS based door opening recommendation system on an embedded Linux system. Technologies: C/C++, cmake, Jenkins",
+        text: "GPS based door opening recommendation system on an embedded Linux system.\nTechnologies: C/C++, cmake, Jenkins",
       },
       {
         color: "#b6b7ba",
         year: "2007 - 2009",
         title: "Software Developer at Siemens",
-        text: 'Distributed real-time service to annotate PDF documents - awarded as "Most-Popular-Feature with the Process-Optimization-Toolchain". Technologies: C#, C++.',
+        text: 'Distributed real-time service to annotate PDF documents - awarded as "Most-Popular-Feature with the Process-Optimization-Toolchain".\nTechnologies: C#, C++.',
       },
       {
         color: "#b6b7ba",
         year: "2007",
-        text: "Master in Computer Science (major), Mathematics (minor) at University of Würzburg. Thesis about Remote-Control in Sensor-Networks.",
+        text: "Master in Computer Science (major), Mathematics (minor) at University of Würzburg. Thesis about Remote-Control in Sensor-Networks.\nTechnologies: C, assembler",
         title: "Master's Degree",
       },
     ],
